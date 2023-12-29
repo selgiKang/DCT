98e6b67c00eb046c8fb81bb4333b8db3f609c19d
kangselgi <k921211@hanmail.net>
2023-12-29 21:40:04 +0900

    map 수정

---
+++ src/src/day20/Q9_map.java
@@ -0,0 +1,24 @@
+package day20;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q9 {
+    public static int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Map<Character, Integer> lastIndexMap = new HashMap<>();
+
+        // 각 문자의 가장 최근 인덱스를 저장
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (lastIndexMap.containsKey(currentChar)) {
+                answer[i] = i - lastIndexMap.get(currentChar);
+            } else {
+                answer[i] = -1;
+            }
+            lastIndexMap.put(currentChar, i);
+        }
+
+        return answer;
+    }
+}
